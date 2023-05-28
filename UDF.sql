331f815ff392c9c25b59fd6930f67201a503e947
Liliana Ribeiro <97979800+lilikas11@users.noreply.github.com>
2023-05-29 00:25:42 +0100

    Update UDF.sql

--- UDF.sql
+++ UDF.sql
@@ -10,3 +10,9 @@ as
 		select ID from Jogador where Email = @email and Senha = @senha
 	)
 go
+
+-- Função para ver se o player tem esmeraldas
+CREATE FUNCTION Compra(@id_personagem int) RETURNS table
+as
+	return(SELECT TOP 1 ID FROM Item WHERE ID_Personagem = @id_personagem AND ID_TipoItem = 90)
+go
