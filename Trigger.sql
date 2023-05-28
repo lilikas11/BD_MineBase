acdea4fb1b58874dd699e3ae4533a256df1aa504
Liliana Ribeiro <97979800+lilikas11@users.noreply.github.com>
2023-05-28 21:04:10 +0100

    Update Trigger.sql

--- Trigger.sql
+++ Trigger.sql
@@ -112,44 +112,6 @@ GO
 
 
 
--- Add do Villager
-CREATE PROCEDURE dbo.create_villager_with_mob (
-    @v_nome VARCHAR(32),
-    @v_personalidade VARCHAR(16),
-    @v_id_bioma INT,
-    @v_dano_facil INT,
-    @v_dano_normal INT,
-    @v_dano_dificil INT,
-    @v_id_tipo_item INT,
-    @v_trabalho VARCHAR(32),
-    @v_id_tipo_item_villager INT
-)
-AS
-BEGIN
-    DECLARE @mob_id INT;
-
-    INSERT INTO Mob(Nome, Personalidade, ID_Bioma, Dano_Facil, Dano_Normal, Dano_Dificil, ID_TipoItem, MortoPor_ID_Personagem)
-    VALUES (@v_nome, @v_personalidade, @v_id_bioma, @v_dano_facil, @v_dano_normal, @v_dano_dificil, @v_id_tipo_item, NULL);
-
-    SET @mob_id = SCOPE_IDENTITY();
-
-    INSERT INTO Villager(ID_Mob, Trabalho, ID_TipoItem)
-    VALUES (@mob_id, @v_trabalho, @v_id_tipo_item_villager);
-
-END;
-
-CREATE TRIGGER Add_Villager_Mob ON dbo.Villager for insert, update
-as
-	begin
-		EXEC dbo.create_villager_with_mob 'New Mob', 'Personality', 1, 10, 20, 30, 1, 'Farmer', 2 ;
-	end
-go
-
-
-
-
-
-
 -- Loggin
 create trigger checkEmailJogador on Jogador for insert, update
 as
