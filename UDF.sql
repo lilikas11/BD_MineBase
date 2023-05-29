628e84bcf2f96c87d8a350de256bdb54e3dfb776
Liliana Ribeiro <97979800+lilikas11@users.noreply.github.com>
2023-05-29 01:24:28 +0100

    Update UDF.sql

--- UDF.sql
+++ UDF.sql
@@ -16,3 +16,23 @@ CREATE FUNCTION Compra(@id_personagem int) RETURNS table
 as
 	return(SELECT TOP 1 ID FROM Item WHERE ID_Personagem = @id_personagem AND ID_TipoItem = 81)
 go
+
+-- ver se o mob tá vivo
+CREATE FUNCTION Morto(@id_mob int) RETURNS int
+AS
+BEGIN
+    DECLARE @status int;
+
+    IF (SELECT ID_Bioma FROM Mob WHERE ID = @id_mob) IS NOT NULL
+    BEGIN
+        -- Mob está vivo
+        SET @status = 1;
+    END
+    ELSE
+    BEGIN
+        -- Mob está morto
+        SET @status = 0;
+    END
+
+    RETURN @status;
+END;
