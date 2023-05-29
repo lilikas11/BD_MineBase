53e2a9cce1e4e097b79b31c40f9872c9333424f6
Liliana Ribeiro <97979800+lilikas11@users.noreply.github.com>
2023-05-29 20:56:50 +0100

    Update View.sql

--- View.sql
+++ View.sql
@@ -1,7 +1,7 @@
 -- inventário
 -- sou muito boa neste videojogo
 CREATE VIEW InventárioView AS
-select 'Item' AS Tipo, TI.Nome AS Nome, I.ID_Personagem
+select I.ID, TI.Nome AS Nome, I.ID_Personagem
 from Item I
 -- aqui faz-se o inner join
 inner join TipoItem TI on I.ID_Personagem = TI.ID
@@ -9,6 +9,11 @@ where I.ID_Personagem is not null
 
 union all
 
-select 'Bloco' as Tipo, B.Nome as Nome, B.ID_Personagem
+select B.ID, B.Nome as Nome, B.ID_Personagem
 FROM Bloco B
 where B.ID_Personagem is not null
+go
+
+
+
+
