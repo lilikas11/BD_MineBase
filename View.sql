aaa36539d9a6f44b83e260991977735f32676b0f
Liliana Ribeiro <97979800+lilikas11@users.noreply.github.com>
2023-05-29 21:16:41 +0100

    Update View.sql

--- View.sql
+++ View.sql
@@ -15,5 +15,26 @@ where B.ID_Personagem is not null
 go
 
 
+-- mundo view para ver as coisas que tão no mundo 
+Create view MundoView AS
 
+	select B.ID, B.Nome, 'Bloco' as Tipo, B.ID_Bioma
+	from Bloco B
+	where B.ID_Bioma is not null
+
+	union all
+
+	-- left join m + v -> se não for v fica null 
+	select M.ID, M.Nome,
+		case when V.ID_Mob is not null 
+			then 'Villager' 
+			else 'Mob'
+			end as Tipo, 
+		M.ID_Bioma
+	from Mob M
+	left join Villager V on M.ID = V.ID_Mob
+	where M.ID_Bioma is not null
+
+	-- os villagers são mobs, mas tem tipo 'Villager'
+go
 
